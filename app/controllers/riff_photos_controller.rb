ce6e9e74eac8a820db4c678f281043f715cb8a8a
Robbi Kenney <robbikenney@gmail.com>
2015-03-14 15:07:22 -0400

    close with paperclip so close

--- app/controllers/riff_photos_controller.rb
+++ app/controllers/riff_photos_controller.rb
@@ -24,7 +24,7 @@ class RiffPhotosController < ApplicationController
 # @http_method XHR POST
 # @url /documents
 def create
-  @riff_photos = current_user.documents.create(params[:photo])
+  @riff_photos = Riff_photo.create
 end
 
   # # POST /riff_photos
@@ -69,9 +69,9 @@ end
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_riff_photo
-      @riff_photo = RiffPhoto.find(params[:id])
-    end
+    # def set_riff_photo
+    #   @riff_photo = RiffPhoto.find(params[:id])
+    # end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def riff_photo_params
