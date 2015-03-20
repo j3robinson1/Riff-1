e0751598548ba9c544fda182868ace8266bf0cef
Chris McCandlish <cmccand@gmail.com>
2015-03-20 00:00:29 -0400

    trying to use ng-upload directive

--- app/assets/javascripts/controllers/controller.js
+++ app/assets/javascripts/controllers/controller.js
@@ -2,20 +2,38 @@
 (function () {
   "use strict";
   angular.module('riffApp')
-  .controller('RiffController', function(RiffService,$location) {
-    var riffCtrl = this;
-    // riffCtrl.riffs = RiffService.getRiffs();
-    riffCtrl.newRiff = {};
+  .controller('RiffController', function ($scope) {
+    $scope.uploadComplete = function (content) {
+      $scope.response = JSON.parse(content); // Presumed content is a json string!
+      $scope.response.style = {
+        fullname: $scope.response.fullname,
+        "font-weight": "bold"
+      };
 
-    RiffService.getRiffs().success(function(data) {
-    riffCtrl.riffs = data;
-    });
-
-    riffCtrl.addMyRiffs = function(newRiff) {
-      RiffService.addRiffs(newRiff);
-      riffCtrl.newRiff = {};
-      $location.path('/');
+      // Clear form (reason for using the 'ng-model' directive on the input elements)
+      $scope.fullname = '';
+      $scope.gender = '';
+      $scope.color = '';
+      // Look for way to clear the input[type=file] element
     };
 
+
   });
+
+  // .controller('RiffController', function(RiffService,$location) {
+  //   var riffCtrl = this;
+  //   // riffCtrl.riffs = RiffService.getRiffs();
+  //   riffCtrl.newRiff = {};
+  //
+  //   RiffService.getRiffs().success(function(data) {
+  //   riffCtrl.riffs = data;
+  //   });
+  //
+  //   riffCtrl.addMyRiffs = function(newRiff) {
+  //     RiffService.addRiffs(newRiff);
+  //     riffCtrl.newRiff = {};
+  //     $location.path('/');
+  //   };
+  //
+  // });
 })();
