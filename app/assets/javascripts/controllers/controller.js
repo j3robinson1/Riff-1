412362879de90c12044d9a319c58f6d2bc56f95d
Chris McCandlish <cmccand@gmail.com>
2015-03-20 15:59:45 -0400

    form data goes to database

--- app/assets/javascripts/controllers/controller.js
+++ app/assets/javascripts/controllers/controller.js
@@ -5,9 +5,6 @@
   .controller('RiffController', function ($scope) {
     $scope.uploadComplete = function (content) {
 
-      // console.log(content);
-
-      console.log(content);
       $scope.response = JSON.parse(content); // Presumed content is a json string!
       $scope.response.style = {
         fullname: $scope.response.fullname,
